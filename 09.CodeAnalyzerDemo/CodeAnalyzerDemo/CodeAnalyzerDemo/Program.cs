09d9b84b4337a7c2cd50d2bbb6b227d9e45eecd8
qatestst <134833768+qatestst@users.noreply.github.com>
2024-03-26 21:16:38 +0200

    new commit

---
+++ 09.CodeAnalyzerDemo/CodeAnalyzerDemo/CodeAnalyzerDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace CodeAnalyzerDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!");
+        }
+
+        private static string MakeGreeting(string name)
+        {
+            // No braces and spacing issues
+            if (name == null) 
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            return "Hello, " + name;
+        }
+    }
+}
---
+++ 09.EsLintDemo/script.js
@@ -0,0 +1,7 @@
+function printHelloWorld() {
+  const someText = "hello world";
+
+  console.log(someText);
+}
+
+printHelloWorld();
